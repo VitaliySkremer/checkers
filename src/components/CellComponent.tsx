ba461993319472899c07206cf89048942e5f4ffb
Сёмкин Виталий Дмитриевич <ivc-SemkinVD@skzd.oao.rzd>
2022-12-20 12:54:02 +0300

    added drugEnter and drugLeave

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -1,4 +1,5 @@
 import {Cell} from "../models/Cell";
+import {useState} from "react";
 
 interface CellProps{
 	cell: Cell;
@@ -8,19 +9,27 @@ interface CellProps{
 
 export const CellComponent = ({cell, selected, click}:CellProps) => {
 
+	const [drugHover, setDrugHover] = useState(false);
 	const onDragOver = (event: React.MouseEvent)=>{
 		if(cell.available){
 			event.preventDefault();
 		}
 	}
 
+	const onDrop = () =>{
+		click(cell);
+		setDrugHover(false);
+	}
+
 	return (
 		<div
-			className={['cell', cell.color, selected ? 'selected': ''].join(' ')}
+			className={['cell', cell.color, selected ? 'selected': '', drugHover && cell.available?'drug__hover':''].join(' ')}
 			onDrag={()=>click(cell)}
-			onDrop={()=>click(cell)}
+			onDrop={onDrop}
 			onDragOver={onDragOver}
 			draggable={!!cell.figure?.logo}
+			onDragEnter={()=>setDrugHover(true)}
+			onDragLeave={()=>setDrugHover(false)}
 			style={{backgroundColor: cell.figure && cell.available ? 'green':''}}
 		>
 			{!cell.figure && cell.available && <i className='available'/>}
